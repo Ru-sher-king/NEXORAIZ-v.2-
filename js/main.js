6c2cb677ede03734d0f74549ad434773e5284a8b
Pedrinux <pedrojav.ariasr@gmail.com>
2026-07-08 17:24:52 -0600

    feature/interfaz

--- js/main.js
+++ js/main.js
@@ -20,12 +20,12 @@ function mostrarModulo(nombreModulo){
         modulos[key].style.display = "none";
         
         const btn = document.getElementById(`btn-${key}`);
-        if(btn) btn.classList.remove("roblox-active-btn");
+        if(btn) btn.classList.remove("btn-activo");
     }
 
     modulos[nombreModulo].style.display = "block";
     modulos[nombreModulo].classList.add("active-modulo");
 
     const btnActivo = document.getElementById(`btn-${nombreModulo}`);
-    if(btnActivo) btnActivo.classList.add("roblox-active-btn");
+    if(btnActivo) btnActivo.classList.add("btn-activo");
 }
